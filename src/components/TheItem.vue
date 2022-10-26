223d601b214ce272ab1c53b8e9d965d9333b9ff7
Anvar <fort2652@gmail.com>
2022-10-26 21:19:06 +0300

    add arrow

--- src/components/TheItem.vue
+++ src/components/TheItem.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="item">
-    <label :for="itemIndex">Item {{ itemIndex + 1 }}</label>
-    <input
-    class="item__checkbox"
-      type="checkbox"
-      :id="itemIndex"
-      v-model="isSelected"
-      @input="$emit('update:isSelected', $event.target.checked)"
-    />
-    <input class="item__number" type="number" v-model="size" @input="$emit('update:size', size)" />
-    <input class="item__color" type="color" v-model="color" @change="$emit('update:color', color)" />
+    <div>
+      <input
+        class="item__checkbox"
+        type="checkbox"
+        :id="itemIndex"
+        v-model="isSelected"
+        @input="$emit('update:isSelected', $event.target.checked)"
+      />
+      <label :for="itemIndex" class="item__label"
+        >Item {{ itemIndex + 1 }}</label
+      >
+    </div>
+    <div>
+      <input
+        class="item__number"
+        type="number"
+        v-model="size"
+        @input="$emit('update:size', size)"
+      />
+      <input
+        class="item__color"
+        type="color"
+        v-model="color"
+        @change="$emit('update:color', color)"
+      />
+    </div>
   </div>
 </template>
 <script>
-import { defineComponent, reactive, ref,watch } from "vue";
+import { defineComponent, reactive, ref, watch } from "vue";
 
 export default defineComponent({
   name: "TheItem",
@@ -32,10 +48,13 @@ export default defineComponent({
     function changeItemColor(color) {
       ctx.emit("update:modelValue", color); // раньше было `this.$emit('input', title)`
     }
-    watch(() => props.item.isSelected,(newVal, oldVal) => {
-      console.log('isSelected + newVal',isSelected.value,' ',newVal)
-      isSelected.value = newVal
-    })
+    watch(
+      () => props.item.isSelected,
+      (newVal, oldVal) => {
+        console.log("isSelected + newVal", isSelected.value, " ", newVal);
+        isSelected.value = newVal;
+      }
+    );
 
     return {
       itemData,
@@ -48,10 +67,20 @@ export default defineComponent({
 </script>
 <style lang="scss" scoped>
 .item {
+  display: flex;
+  justify-content: space-around;
   &__number {
     width: 30px;
     padding: 2px;
     text-align: center;
+  }
+
+  &__checkbox {
+    width: auto;
+    min-width: 0;
+  }
+  &__label {
+    margin-right: 10px;
   }
   &__color {
     width: auto;
--- src/components/TheSelectList.vue
+++ src/components/TheSelectList.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="select-list">
-    <ul
-      class="select-list__list"
-      v-for="(list, listIndex) in lists"
-      :key="listIndex"
-    >
-      <label :for="listIndex">List {{ listIndex + 1}}</label>
-      <input :class="{'select-list__list-checkbox': true, 'partially': list.addStyle }"  ref="input" :id="listIndex" type="checkbox" v-bind="list.isSelected" :checked="list.isSelected" @change="handleChange({listIndex, checked: $event.target.checked})" />
+  <div class="select-lists" v-for="(list, listIndex) in lists" :key="listIndex">
+    <div :class="{'select-lists__arrow-open': true, 'arrow-close': isVisibleList[listIndex]}" @click="handleOpenList(listIndex)"></div>
+   
+    <input
+      :class="{ 'select-lists__list-checkbox': true, partially: list.addStyle }"
+      ref="input"
+      :id="listIndex"
+      type="checkbox"
+      v-bind="list.isSelected"
+      :checked="list.isSelected"
+      @change="handleChange({ listIndex, checked: $event.target.checked })"
+    />
+    <label :for="listIndex" class="select-lists__label">List {{ listIndex + 1 }}</label>
+    <ul class="select-lists__list" v-show="isVisibleList[listIndex]">
       <li v-for="(item, itemIndex) in list.items" :key="itemIndex">
         <the-item
           :item="item"
@@ -20,7 +26,6 @@
   </div>
 </template>
 <script>
-
 import { computed, defineComponent, ref, watch } from "vue";
 import TheItem from "./TheItem.vue";
 
@@ -28,19 +33,21 @@ export default defineComponent({
   components: { TheItem },
   name: "TheSelectList",
   props: ["lists"],
-  emits: ['selectAll'],
+  emits: ["selectAll"],
   setup(props, ctx) {
-    const input = ref()
-    const checkedItems = ref([]);
-    const checkedLists = computed(() => {
-      lists
-    })
+    const input = ref();
+    const isVisibleList = ref([false,false,false,false,false,false,false]);
+
+    const handleOpenList = (indx) => {
+      isVisibleList.value[indx] = !isVisibleList.value[indx]
+    }
     const isListChecked = computed(() => {
-      return props.lists.filter(list => list.isSelected === true
-      ? list.items.forEach((item) => item.isSelected = true )
-      : list
-      )
-    })
+      return props.lists.filter((list) =>
+        list.isSelected === true
+          ? list.items.forEach((item) => (item.isSelected = true))
+          : list
+      );
+    });
     // const lists = reactive([
     //   {
     //     isSelected: false,
@@ -75,29 +82,28 @@ export default defineComponent({
     // ]);
 
     const handleChange = (params) => {
-      ctx.emit('selectAll', params)
-      if(props.lists[params.listIndex].isSelected === false){
-        props.lists[params.listIndex].isSelected = params.checked
-      props.lists[params.listIndex].items.forEach(item => {
-        item.isSelected = true
-      })
-      } else if (props.lists[params.listIndex].isSelected === true){
-        props.lists[params.listIndex].isSelected = params.checked
-      props.lists[params.listIndex].items.forEach(item => {
-        item.isSelected = false
-      })
+      ctx.emit("selectAll", params);
+      if (props.lists[params.listIndex].isSelected === false) {
+        props.lists[params.listIndex].isSelected = params.checked;
+        props.lists[params.listIndex].items.forEach((item) => {
+          item.isSelected = true;
+        });
+      } else if (props.lists[params.listIndex].isSelected === true) {
+        props.lists[params.listIndex].isSelected = params.checked;
+        props.lists[params.listIndex].items.forEach((item) => {
+          item.isSelected = false;
+        });
       }
-
-    }
+    };
 
     // watch(() => props.lists,() => {
-      
+
     //   props.lists.forEach(list => {
     //     if(list.isSelected === true){
     //       list.items.forEach(item => {
     //         item.isSelected = true
     //       })
-    //     } 
+    //     }
     //     // else if(list.isSelected === false){
     //     //   list.items.forEach(item => {
     //     //     item.isSelected = false
@@ -109,53 +115,84 @@ export default defineComponent({
     // },
     // { deep: true })
 
-    watch(() => props.lists, (newVal, oldVal) => {
-      console.log('(newVal, oldVal)',newVal, " /n" ,oldVal)
-      console.log('input',input.value)
-      props.lists.forEach(list => {
-        if (list.items.some(item => item.isSelected === true)){
-        list.addStyle = true
-      }
-      if (list.items.every(item => item.isSelected === false)){
-        list.addStyle = false
-      }
-        if(list.items.every(item => item.isSelected === true)){
-          list.addStyle = false
-        list.isSelected = true
-      }  else if (list.items.some(item => item.isSelected === false)) {
-        list.isSelected = false
-        // list.addStyle = false
-      }
-
-      })
-    },{deep: true})
+    watch(
+      () => props.lists,
+      (newVal, oldVal) => {
+        console.log("(newVal, oldVal)", newVal, " /n", oldVal);
+        console.log("input", input.value);
+        props.lists.forEach((list) => {
+          if (list.items.some((item) => item.isSelected === true)) {
+            list.addStyle = true;
+          }
+          if (list.items.every((item) => item.isSelected === false)) {
+            list.addStyle = false;
+          }
+          if (list.items.every((item) => item.isSelected === true)) {
+            list.addStyle = false;
+            list.isSelected = true;
+          } else if (list.items.some((item) => item.isSelected === false)) {
+            list.isSelected = false;
+            // list.addStyle = false
+          }
+        });
+      },
+      { deep: true }
+    );
     return {
       input,
-      checkedItems,
-      handleChange
+      isVisibleList,
+      handleOpenList,
+      handleChange,
     };
   },
 });
 </script>
 <style lang="scss" scoped>
-.select-list {
+.select-lists {
+  padding: 10px;
+  position: relative;
   &__list-checkbox {
     position: relative;
     min-width: 0;
     padding: 0;
+    padding-left: 30px;
+    margin-left: 20px;
+    transition: all ease-in 0.2s;
+  }
+
+  &__arrow-open {
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    left: 10px;
+    top: 15px;
+    padding: 5px;
+    border-bottom: 2px solid #000;
+    border-right: 2px solid #000;
+    transform: rotate(-45deg);
+    cursor: pointer;
     transition: all ease-in .2s;
   }
+
+
+  &__label {
+    
+  }
 }
+
+.arrow-close {
+    transform: rotate(45deg);
+  }
 .partially {
   &::after {
     position: absolute;
-    content: '';
-  left: 3px;
-  top: 2px;
-  width: 6px;
-  height: 7px;
-  background-color: rgb(0, 153, 153);
-  // border-radius: 50%;
+    content: "";
+    left: 3px;
+    top: 2px;
+    width: 6px;
+    height: 7px;
+    background-color: rgb(0, 153, 153);
+    // border-radius: 50%;
   }
 }
 </style>
